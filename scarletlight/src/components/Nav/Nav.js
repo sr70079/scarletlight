c314356e97406888fdc8eab7a73560eaef3db998
Sarah Marshall <sr70079@gmail.com>
2021-07-20 17:04:38 -0400

    image folder and nav bar

--- scarletlight/src/components/Nav/Nav.js
+++ scarletlight/src/components/Nav/Nav.js
@@ -1,37 +1,113 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import logo from "./images/logo.jpg"
+// import ResponsiveImgMaterialUi from 'responsive-img-material-ui';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  toolbar: {
+    minHeight: 180,
+    alignItems: 'flex-start',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+  },
   title: {
     flexGrow: 1,
+    alignSelf: 'flex-end',
+  },
+  logo: {
+      backgroundImage: `url(${logo})`,
+      backgroundRepeat: 'no-repeat',
+    //   backgroundAttachment: 'fixed',
+      backgroundPosition: 'center',
+      backgroundColor: 'white',
+    //   backgroundPosition: "center center",
+    // backgroundSize: "cover",
+
+
   },
 }));
 
-export default function ButtonAppBar() {
+export default function ProminentAppBar() {
   const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <AppBar  className={classes.logo}>
+          {/* <ResponsiveImgMaterialUi xs="/img/logo-small.jpg" md="/img/logo-medium.jpg" lg="/img/logo-large.jpg" /> */}
+        <Toolbar className={classes.toolbar} >
+            
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            // color="inherit"
+            aria-label="open drawer"
+            onClick={handleMenu}
+          >
             <MenuIcon />
           </IconButton>
+
           <Typography variant="h6" className={classes.title}>
-            Scarlet Light Photography
+            
           </Typography>
+
+          <IconButton 
+            aria-label="display more actions" 
+            edge="end" 
+            // color="inherit"
+            >
+            <MoreIcon />
+          </IconButton>
+
+          {/* <div>
+                <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+          </div> */}
+
         </Toolbar>
       </AppBar>
     </div>
