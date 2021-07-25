a2adf1e6b1235b29fdf10dbb6c65b7a9091a953f
Sarah Marshall <sr70079@gmail.com>
2021-07-25 11:46:57 -0400

    theme.js added to control MUI theme within components

--- scarletlight/src/App.js
+++ scarletlight/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Calendar from "./pages/Calendar";
+import Landing from "./pages/Landing";
+import Portfolio from "./pages/Portfolio";
+import Pricing from "./pages/Pricing";
+
 import Nav from "./components/Nav/Nav";
 import StickyFooter from "./components/StickyFooter/StickyFooter";
-// import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper/Wrapper";
 
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme'
+
+import Item from './components/Item/item'
+
+
 
 // import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 // import { amber, teal } from '@material-ui/core/colors';
@@ -23,26 +35,29 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
-    <Router>
-      <div>
-        {/* <ThemeProvider theme={theme}> */}
-          <CssBaseline />
-          <Nav />
-          {/* <Route exact path="/about" component={About} /> */}
-          {/* <Wrapper> */}
-            {/* <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/pricing" component={Pricing} />
-            <Route exact path="/calendar" component={Calendar} /> 
-            <Route exact path="/contact" component={Contact} />  */}
-          {/* </Wrapper> */}
-          <StickyFooter />
-        {/* </ThemeProvider> */}
-      </div>
-    </Router>
-    // </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+            <CssBaseline />
+            <Nav />
+            {/* <Item /> */}
+             
+
+
+            <Wrapper>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/portfolio" component={Portfolio} />
+              <Route exact path="/pricing" component={Pricing} />
+              <Route exact path="/calendar" component={Calendar} /> 
+              <Route exact path="/contact" component={Contact} /> 
+            </Wrapper>
+
+            <StickyFooter />
+            
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ scarletlight/src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme }  from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: { 500: '#467fcf' },
+  },
+})
+
+export default theme
